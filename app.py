ee89160475dfd4aa4e43ad8a3024d7a996dec7e2
sourci91 <sorg.michael@outlook.com>
2025-04-21 12:48:16 +0200

    app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 st.set_page_config(page_title="Energiedashboard", layout="wide")
  
-st.title("🔆 Marktwert Solar (Monatlich, 2024)")
+st.title("🔆 Marktwert Solar (Monatlich, 2025)")
  
 # CSV von Netztransparenz.de laden
 url = "https://raw.githubusercontent.com/sourci91/Energie-Dashboard/refs/heads/main/Monatsmarktwerte%20%5B2025-04-21%2012-43-37%5D.csv"
